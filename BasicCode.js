1e01e961b2c4494466d2a8b4d9fff34bc83b08ad
Naveenkumar020599 <75249503+Naveenkumar020599@users.noreply.github.com>
2026-01-22 12:26:11 +0530

    Add files via upload

---
+++ BasicCode.js
@@ -0,0 +1,12 @@
+let userName="Naveen"
+let organization="testleaf"
+let contactNumber = 98408234356
+let knowsAutomation = true
+let usesPlaywright
+
+
+console.log("First Name is " +userName +" "+ typeof(userName))
+console.log("Company Name is " +organization +" "+ typeof(organization))
+console.log("Mobile Number is " +contactNumber +" "+ typeof(contactNumber))
+console.log("Is Automation " +knowsAutomation +" "+ typeof(knowsAutomation))
+console.log("Has Playwright " +usesPlaywright)
